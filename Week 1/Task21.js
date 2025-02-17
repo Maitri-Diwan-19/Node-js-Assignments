06bae1ee70ccb913dacdfa1226f1acf8430ac615
MAITRI DIWAN <maitridiwan19@gmail.com>
2025-02-17 11:30:40 +0530

    commit

--- Week 1/Task21.js
+++ Week 1/Task21.js
@@ -3,28 +3,25 @@
 //  The function should log whether the objects are equal or not based on their properties.
 //  Test the function with user1 and user2
 
-
 const user1 = {
-    name: "maitri",
-    age: 20,
-    city: "Ahmedabad"
-  };
-  
-  const user2 = {
-    name: "maitri",
-    age: 20,
-    city: "Ahmedabad"
-  };
-  
-  // Function to compare objects using JSON.stringify
-  function compareObjects(obj1, obj2) {
-    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
-      console.log("Objects are equal");
-    } else {
-      console.log("Objects are not equal");
-    }
+  name: "maitri",
+  age: 20,
+  city: "Ahmedabad",
+};
+
+const user2 = {
+  name: "maitri",
+  age: 20,
+  city: "Ahmedabad",
+};
+
+// Function to compare objects using JSON.stringify
+function compareObjects(obj1, obj2) {
+  if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+    console.log("Objects are equal");
+  } else {
+    console.log("Objects are not equal");
   }
-  
-  
-  compareObjects(user1, user2);
-  
+}
+
+compareObjects(user1, user2);
--- Week 1/Task23.js
+++ Week 1/Task23.js
@@ -2,3 +2,17 @@
 //  Add a method greet to the prototype of the constructor, which logs a greeting message using the person's name.
 //  Instantiate two objects using the Person constructor and call the greet method on both.
 
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+
+  Person.prototype.greet = function () {
+    console.log(`Hello my name is ${this.name}`);
+  };
+}
+
+const person1 = new Person("Maitri", 20);
+const person2 = new Person("Binal", 25);
+
+person1.greet();
+person2.greet();
